4711a63afe7fd24c71839e95b29bf82f551d0b9e
Fonzy57 <traben@hotmail.fr>
2025-03-21 14:01:51 +0100

    Change accueil page

--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-accueil',
-  imports: [],
   templateUrl: './accueil.component.html',
-  styleUrl: './accueil.component.scss',
+  styleUrls: ['./accueil.component.scss'],
+  imports: [MatButtonModule, MatSnackBarModule],
 })
 export class AccueilComponent {
-  titre = 'Vous pouvez me cliquer :)';
-  compteur = 0;
+  http = inject(HttpClient);
+  produits: any = [];
 
-  onClick() {
-    this.compteur++;
+  private _snackBar = inject(MatSnackBar);
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      panelClass: 'my-custom-snackbar',
+    });
   }
+
+  ngOnInit() {
+    this.http
+      .get<any[]>('http://localhost:8080/produits')
+      .subscribe((listeProduits) => {
+        this.produits = listeProduits;
+      });
+  }
+
+  onClickBuy = (produit: any) => {
+    this.openSnackBar(`Je viens d'acheter une ${produit.nom}`, 'x');
+  };
 }
