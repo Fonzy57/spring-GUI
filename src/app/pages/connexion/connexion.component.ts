d1fcccdcc4fd598df36f31d78b94bb7c357aff88
Fonzy57 <traben@hotmail.fr>
2025-04-10 16:21:39 +0200

    Add connexion page

--- src/app/pages/connexion/connexion.component.ts
+++ src/app/pages/connexion/connexion.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-connexion',
-  imports: [],
+  imports: [MatButtonModule, MatInputModule, FormsModule, ReactiveFormsModule],
   templateUrl: './connexion.component.html',
-  styleUrl: './connexion.component.scss'
+  styleUrl: './connexion.component.scss',
 })
 export class ConnexionComponent {
+  notification = inject(MatSnackBar);
+  http = inject(HttpClient);
+  formBuilder: FormBuilder = inject(FormBuilder);
 
+  formulaire = this.formBuilder.group({
+    email: ['user@toto.fr', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required]],
+  });
+
+  onConnection(): void {
+    if (this.formulaire.valid) {
+      // Ici faire un try catch pour être sûr que l'API est joignable
+      this.http
+        .post('http://localhost:8080/connexion', this.formulaire.value, {
+          responseType: 'text',
+        })
+        .subscribe({
+          next: (result) => console.log(result), // ICI LE RESULT C'EST LE JWT
+          error: (erreur) => {
+            if (erreur.status === 401) {
+              this.notification.open('Mauvais email ou mot de passe', '', {
+                duration: 5000,
+                verticalPosition: 'top',
+              });
+            }
+          },
+        });
+    }
+  }
 }
