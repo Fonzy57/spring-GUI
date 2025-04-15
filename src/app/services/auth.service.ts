2c1fc3b10199ada79f1534f93e97208da4f3413d
Fonzy57 <traben@hotmail.fr>
2025-04-15 12:02:05 +0200

    Add jwt decode

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,20 +5,37 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
   connecte = false;
+  role: string | null = null;
 
   constructor() {
     const jwt: string | null = localStorage.getItem('jwt');
 
-    this.connecte = jwt !== null;
+    if (jwt !== null) {
+      this.decodeJwt(jwt);
+    }
   }
 
-  connexion(jwt: string) {
+  // Init user infos (get jwt and role)
+  decodeJwt(jwt: string) {
     localStorage.setItem('jwt', jwt);
+
+    // On récupère la partie "body" du JWT
+    const middleOfJWT = jwt.split('.')[1];
+
+    // on décode la base 64
+    const jwtBody = atob(middleOfJWT);
+
+    // On transforme le json en objet JS
+    const body = JSON.parse(jwtBody);
+
+    this.role = body.role;
+
     this.connecte = true;
   }
 
   deconnexion() {
     localStorage.removeItem('jwt');
     this.connecte = false;
+    this.role = null;
   }
 }
